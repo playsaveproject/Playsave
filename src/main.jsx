62e8f7ab8794e90fb36caabf3b4d213c1ef4e3e7
martinlgalvan <martinlgalvan@yahoo.com>
2025-06-19 17:08:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
+import './index.css';
+
 
 const domain   = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
